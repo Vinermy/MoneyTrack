9f142b08a65bb8d65e77a0b7667a3c2a15e5c5fc
Vinermy <egorkosachev@vk.com>
2024-01-24 22:14:23 +0700

    Created the index page layout, started making the settings page

--- moneytrack_app/urls.py
+++ moneytrack_app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from moneytrack_app import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('/settings', views.settings, name='settings')
 ]
